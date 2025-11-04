ca21fb19ae96f107000c6c39873666498a008553
shiv-0831 <shivamgarg0831@gmail.com>
2025-11-04 12:03:32 +1000

    moving to integrate ADNI dataset

--- recognition/adni_convnext_47280647/modules.py
+++ recognition/adni_convnext_47280647/modules.py
@@ -7,19 +7,18 @@ class TinyCNN(nn.Module):
     def __init__(self, in_chans: int = 1, num_classes: int = 2, head_dropout: float = 0.0):
         super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(in_chans, 16, 3, padding=1), nn.ReLU(inplace=True), nn.MaxPool2d(2),   # 112×112
-            nn.Conv2d(16, 32, 3, padding=1),        nn.ReLU(inplace=True), nn.MaxPool2d(2),   # 56×56
+            nn.Conv2d(in_chans, 16, 3, padding=1), nn.ReLU(inplace=True), nn.MaxPool2d(2),  # 112x112
+            nn.Conv2d(16, 32, 3, padding=1),        nn.ReLU(inplace=True), nn.MaxPool2d(2),  # 56x56
             nn.Conv2d(32, 64, 3, padding=1),        nn.ReLU(inplace=True),
-            nn.AdaptiveAvgPool2d((1, 1))                                                # 64×1×1
+            nn.AdaptiveAvgPool2d((1, 1))                                                # 64x1x1
         )
         self.dropout = nn.Dropout(p=head_dropout) if head_dropout > 0 else nn.Identity()
         self.head = nn.Linear(64, num_classes)
 
     def forward(self, x):
-        x = self.features(x)               # (B,64,1,1)
-        x = x.flatten(1)                   # (B,64)
+        x = self.features(x).flatten(1)
         x = self.dropout(x)
-        logits = self.head(x)              # (B,2)
+        logits = self.head(x)
         probs = F.softmax(logits, dim=1)
         return {"logits": logits, "probs": probs}
 
